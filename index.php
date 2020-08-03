9c63641e01b19eaca1980fe3d9af8501a9cfbc2b
Nova Saputra <nova.saputra@gmail.com>
2020-08-03 09:01:37 +0700

    add database call function

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Dotenv\Dotenv;
 use Monolog\Logger;
 use Monolog\Handler\RotatingFileHandler;
 use Psaux\Util\Timer;
+use Psaux\Config\DatabaseConfig;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -18,5 +19,23 @@ $start = $timer->start();
 sleep(1);
 $time = $timer->get_time();
 $log->info("Hello World! Total: $time");
+
+$conf = new DatabaseConfig($log, $_ENV['DB_APP_HOST'],$_ENV['DB_APP_USER'],$_ENV['DB_APP_PASS'],$_ENV['DB_APP_NAME']);
+$pdo = $conf->getPdo();
+try {
+  $sql = "SELECT NOW() as currenttime";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $result = $stmt->fetch();
+  if(!empty($result)) {
+    $time = $result['currenttime'];
+    print "Database time is $time";
+    $log->info("Database time is $time!");
+  } else {
+    $log->info("Error in query!");
+  }
+} catch (PDOException $e) {
+  $this->log->error('PDOException: '.$e->getMessage());
+}
 
 ?>
